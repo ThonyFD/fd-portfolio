a35f907a9653afda039cb4e5e72c4cb0526e7dc0
thonyfd <amoquera@wicon.io>
2023-11-12 16:31:20 -0500

    firebase variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  portfolio: 'portfolio',
   production: true,
-  version: require('../../package.json').version
+  version: require('../../package.json').version,
+  firebase: {
+    apiKey: "AIzaSyA6ssOUYQlKjX7dCfGfDmvUuJ22dPBIX1c",
+    authDomain: "thony-test.firebaseapp.com",
+    databaseURL: "https://thony-test.firebaseio.com",
+    projectId: "thony-test",
+    storageBucket: "thony-test.appspot.com",
+    messagingSenderId: "860149478663",
+    appId: "1:860149478663:web:4c94b27d9d591ba36841a2"
+  }
 };
